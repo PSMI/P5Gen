64f7f0045e8a0ff9af109e56bb1bfd4dc0eb28f3
Oliver Candelario <ofcandelario@philweb.com.ph>
2014-03-16 22:04:30 +0800

    Added Customer Service Account Type

--- protected/models/AccountTypes.php
+++ protected/models/AccountTypes.php
@@ -16,7 +16,7 @@ class AccountTypes extends CFormModel
     {
         $connection = $this->_connection;
         
-        $sql = "SELECT * FROM ref_account_types WHERE account_type_id != 3;";
+        $sql = "SELECT * FROM ref_account_types WHERE account_type_id != 3 AND account_type_id != 1;";
         $command = $connection->createCommand($sql);
         $result = $command->queryAll();
         
